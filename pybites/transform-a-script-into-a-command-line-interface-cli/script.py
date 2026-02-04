9136e6e09d1001517ad6f743dbe465ed94ed1f7f
Manny Savage <manny@savage.cx>
2026-02-04 23:05:27 +1000

    [ES] Python training - 20260204

---
+++ pybites/transform-a-script-into-a-command-line-interface-cli/script.py
@@ -0,0 +1,20 @@
+from typing import Annotated
+
+import typer
+
+
+def sum_numbers(a: int, b: int):
+    """Sums two numbers"""
+    return a + b
+
+
+def main(
+    a: Annotated[int, typer.Argument(help="The value of the first summand")],
+    b: Annotated[int, typer.Argument(help="The value of the second summand")],
+):
+    """CLI that allows you to add two numbers"""
+    print(sum_numbers(a, b))
+
+
+if __name__ == "__main__":
+    typer.run(main)
